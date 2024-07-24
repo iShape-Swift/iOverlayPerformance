af1ffd571234fcd5a488b86350f66a2fbb7dc543
Nail Sharipov <nailxsharipov@gmail.com>
2024-07-24 16:48:57 +0300

    update results

--- iOverlayPerformance/ManySquaresTest.swift
+++ iOverlayPerformance/ManySquaresTest.swift
@@ -13,20 +13,22 @@ import Foundation
 // 3 GHz 6-Core Intel Core i5, 40 GB 2667 MHz DDR4
 
 // master
-// 125 - 0.531622052192688
-// 250 - 2.0265350341796875
-// 500 - 7.711843967437744
-// 1000 - 30.740682005882263
-
 // 125 - 0.43583405017852783
 // 250 - 1.8057969808578491
 // 500 - 7.400161981582642
 // 1000 - 30.35614001750946
+
+// 125 - 0.23533731326460838
+// 250 - 0.9990083128213882
+// 500 - 4.50067526102066
+// 1000 - 19.30902099609375
+
+
 struct ManySquaresTest {
     
     func run() {
 
-        let n = 3
+        let n = 1000
         
         let subjPaths = self.manySuares(
             start: .zero,
@@ -48,7 +50,7 @@ struct ManySquaresTest {
         
         for _ in 0..<it_count {
             let overlay = Overlay(subjShape: subjPaths, clipShape: clipPaths)
-            let graph = overlay.buildGraph(solver: Solver.list)
+            let graph = overlay.buildGraph(solver: Solver.auto)
             _ = graph.extractShapes(overlayRule: .xor, minArea: 0)
         }
 
